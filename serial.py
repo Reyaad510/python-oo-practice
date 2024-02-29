cb4c60c42167870f3f96dcd09dad4392c7b8b40b
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2024-02-29 08:26:18 -0500

    serial.py file completed

--- serial.py
+++ serial.py
@@ -4,6 +4,7 @@ class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
     
     >>> serial = SerialGenerator(start=100)
+    
 
     >>> serial.generate()
     100
@@ -20,3 +21,27 @@ class SerialGenerator:
     100
     """
 
+
+    """Initialize the SerialGenerator with a starting value."""
+    def __init__ (self, start = 100):
+        self.start = start
+        self.current = start
+
+    """Generate the next serial number."""
+    def generate(self):
+        serial_number = self.current
+        self.current += 1
+        return serial_number
+    
+    """Reset the serial number generator to its initial state."""
+    def reset(self):
+        self.current = self.start
+        serial_number = self.current
+        return serial_number
+
+
+
+
+# serial = SerialGenerator()
+
+# print(serial.generate())
